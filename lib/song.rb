25676fcafad1a1f6f5fad90a426db8d7ba237c19
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2023-02-22 08:40:50 +0300

    done

--- lib/song.rb
+++ lib/song.rb
@@ -7,4 +7,37 @@ class Song
     @album = album
   end
 
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS song (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      album TEXT
+    )
+    SQL
+
+    DB[:conn].execute(sql)
+
+  end
+
+  def save 
+    sql = <<-SQL
+    INSERT INTO songs (name, album)
+    VALUES(?, ?)
+    SQL
+
+    #insert the song, ..a new row into the database
+    DB[:conn].execute(sql, self.name, self.album)
+
+    #get song id from the database and save it to the ruby instance
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+
+    #return the ruby instance
+    self
+  end
+
+  def self.create(name:, album:)
+  song = Song.new(name: name, album: album)
+  song.save
+  end
 end
